a95a4ef2e57004dc238bb1237e0ced0d55731fb8
Hamed Saeedifard <hameds@gmail.com>
2018-05-07 18:19:46 +0430

    add SequentialMediator

---
+++ Araye.Code.Core/Araye.Code.Core/Events/SequentialMediator.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Araye.Code.Core.Events
+{
+    public class SequentialMediator : Mediator
+    {
+        public SequentialMediator(SingleInstanceFactory singleInstanceFactory, MultiInstanceFactory multiInstanceFactory) : base(singleInstanceFactory, multiInstanceFactory)
+        {
+        }
+
+        protected async override Task PublishCore(IEnumerable<Task> allHandlers)
+        {
+            foreach (var handler in allHandlers)
+            {
+                await handler;
+            }
+        }
+    }
+}
