9482f8e362248edc92717a9d319f9cf6f83e8b6b
Hamed Saeedifard <hameds@gmail.com>
2018-05-07 15:26:29 +0430

    initial code commit for modularity

--- Araye.Code.Core/Araye.Code.Core/Extensions/CollectionExtensions.cs
+++ Araye.Code.Core/Araye.Code.Core/Extensions/CollectionExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace Araye.Code.Core.Extensions
 {
@@ -28,6 +30,38 @@ namespace Araye.Code.Core.Extensions
                          group name by i++ / chunkSize into part
                          select part.AsEnumerable();
             return chunks;
+        }
+
+        public static IQueryable<T> OrderByName<T>(this IQueryable<T> source, string propertyName, bool isDescending)
+        {
+            if (source == null)
+            {
+                throw new ArgumentException("source");
+            }
+
+            if (string.IsNullOrWhiteSpace(propertyName))
+            {
+                throw new ArgumentException("propertyName");
+            }
+
+            Type type = typeof(T);
+            ParameterExpression arg = Expression.Parameter(type, "x");
+            PropertyInfo propertyInfo = type.GetProperty(propertyName);
+            Expression expression = Expression.Property(arg, propertyInfo);
+            type = propertyInfo.PropertyType;
+
+            Type delegateType = typeof(Func<,>).MakeGenericType(typeof(T), type);
+            LambdaExpression lambda = Expression.Lambda(delegateType, expression, arg);
+
+            var methodName = isDescending ? "OrderByDescending" : "OrderBy";
+            object result = typeof(Queryable).GetMethods().Single(
+                method => method.Name == methodName
+                        && method.IsGenericMethodDefinition
+                        && method.GetGenericArguments().Length == 2
+                        && method.GetParameters().Length == 2)
+                .MakeGenericMethod(typeof(T), type)
+                .Invoke(null, new object[] { source, lambda });
+            return (IQueryable<T>)result;
         }
     }
 }
